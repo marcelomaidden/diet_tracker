a5632f3ae61f6311663ec249ac0aa23c8a57b628
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-03-22 13:15:34 -0300

    Adds categories photos

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ Doorkeeper::Application.create(name: "Android client", redirect_uri: "", scopes:
 curl -X POST -d "email=marcelomaidden@gmail.com" -d "password=57iqf68prj" -d "grant_type=password" -d "client_id=secret_uid" -d "client_secret=secret_key" http://localhost:3000/oauth/token
 
 
+## Acknowledgments
+[https://unsplash.com/@sigmund]@sigmund - Unsplash (Carbohydrate photo)
+[https://unsplash.com/@tempestdesigner]@tempestdesigner - Unsplash (Protein photo)
+[https://unsplash.com/@enginakyurt]@enginakyurt - Unsplash (Fat photo)
+[https://br.depositphotos.com/]Deposit photos
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,6 @@ photo: 'https://st3.depositphotos.com/15648834/17930/v/1600/depositphotos_179308
 User.create(name: "Luana", email: "luana@test.com", password: "123456", 
 photo: 'https://st3.depositphotos.com/15648834/17930/v/1600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg')
 
-Category.create(name: "Carbohydrates")
-Category.create(name: "Proteins")
-Category.create(name: "Fats")
+Category.create(name: "Carbohydrates", photo: 'https://images.unsplash.com/photo-1572969690789-1832ec2792ae?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=384&q=80%20384w')
+Category.create(name: "Proteins", photo: 'https://images.unsplash.com/photo-1532550907401-a500c9a57435?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80')
+Category.create(name: "Fats", photo: 'https://images.unsplash.com/photo-1600841889672-d3387c10c7d4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80 750w')
