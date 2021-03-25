a49f2133a50f81db28afc59185e1117363daaebf
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-03-25 14:33:45 -0300

    Changes import due to precedence

--- app/javascript/packs/src/components/Dashboard.test.js
+++ app/javascript/packs/src/components/Dashboard.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { render } from '../test/testUtils';
 import { screen } from '@testing-library/react';
+import { render } from '../test/testUtils';
 import Dashboard from './Dashboard';
 
 it('Renders the connected app with initialState', () => {
--- app/javascript/packs/src/components/Menu.test.js
+++ app/javascript/packs/src/components/Menu.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { render } from '../test/testUtils';
 import { screen } from '@testing-library/react';
+import { render } from '../test/testUtils';
 import Menu from './Menu';
 
 it('Renders the Menu component with User info', () => {
